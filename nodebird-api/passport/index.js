480101a2b1eaacae681c3b0c913881b171dd5e9c
hankkuu <hankkuu@gmail.com>
2022-09-06 19:42:17 +0900

    api 서버 추가

---
+++ nodebird-api/passport/index.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const User = require('../models/user');
+const Post = require('../models/post');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({ 
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings'
+      }, {
+        model: Post,
+        as: 'LikePosts'
+      }]
+     })
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+
+  local();
+  kakao();
+};
---
+++ nodebird-api/routes/auth.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
+  const { email, nick, password } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.redirect('/join?error=exist');
+    }
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email,
+      nick,
+      password: hash,
+    });
+    return res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect('/');
+    });
+  })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
+});
+
+
+/**
+ * passport 0.6.0 업그레이드 후 수정 내용
+ */
+router.get('/logout', isLoggedIn, (req, res) => {
+  // https://stackoverflow.com/questions/72336177/error-reqlogout-requires-a-callback-function
+  req.logout(function(err) {
+        if (err) { return next(err) }
+        req.session.destroy();
+
+        /** 
+         * 카카오 로그아웃 구현하기
+         */
+        // "https://kauth.kakao.com/oauth/logout?client_id=${YOUR_REST_API_KEY}&logout_redirect_uri=${YOUR_LOGOUT_REDIRECT_URI}"
+
+
+        res.redirect('/');
+  });
+});
+
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get('/kakao/callback', passport.authenticate('kakao', {
+  failureRedirect: '/',
+}), (req, res) => {
+  res.redirect('/');
+});
+
+
+
+
+
+module.exports = router;
