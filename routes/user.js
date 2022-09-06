b095d83a13819714275b7cc69df77bbd954e3672
hankkuu <hankkuu@gmail.com>
2022-09-07 08:00:37 +0900

    user controller 분리

--- routes/user.js
+++ routes/user.js
@@ -5,20 +5,7 @@ const User = require('../models/user');
 
 const router = express.Router();
 
-router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
-    if (user) {
-      await user.addFollowing(parseInt(req.params.id, 10));
-      res.send('success');
-    } else {
-      res.status(404).send('no user');
-    }
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
+router.post('/:id/follow', isLoggedIn, addFollowing);
 
 
 router.delete('/:id/follow', isLoggedIn, async (req, res, next) => {
